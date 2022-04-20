ff00afbeb10d4529b810a04c788f2a16721bf5c9
Apw STI <apwsti@outlook.com>
2022-04-20 18:04:46 -0300

    Add Apw.Lab.AWSSQS.Consumer

---
+++ src/Apw.Lab.AWSSQS.Consumer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+
+namespace Apw.Lab.AWSSQS.Consumer
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            SQSMessageConsumer sqsConsumer = new SQSMessageConsumer();
+
+            await sqsConsumer.Listen();
+
+        }
+    }
+}
